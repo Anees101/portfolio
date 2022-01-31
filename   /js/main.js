9d5686b0ab25a285513f97a1290dc1beb325f551
Ahmad Anees <83137173+Anees101@users.noreply.github.com>
2022-01-31 08:52:44 +0000

    Update main.js

---   /js/main.js
+++   /js/main.js
@@ -254,3 +254,22 @@
   });
 
 })()
+
+
+
+
+
+
+
+//Age
+function age() {
+
+var birth = new Date('2003-01-16');
+var now = new Date();
+var difference = now - birth;
+// Dates are set from 1970, so remove that to get the year 
+var age = new Date(difference).getFullYear() - 1970
+
+var span = document.getElementById('age');
+span.innerHTML = age;
+}
